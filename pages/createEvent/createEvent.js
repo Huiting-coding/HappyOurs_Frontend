61a83cc832c8edce3a8ea7cfb0300ebfa36f5d83
Huiting Qiu <277080574@qq.com>
2022-03-14 23:51:45 +0800

    add if statement to the time picker

--- pages/createEvent/createEvent.js
+++ pages/createEvent/createEvent.js
@@ -26,7 +26,21 @@ Page({
       }
     })
   },
-
+  notice: function(){
+    console.log("shihsikan")
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+    
+  },
   formSubmit: function(e) {
     console.log('e.detail', e.detail)
     const page = this
